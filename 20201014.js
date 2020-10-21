acd6f91950539c7c95c63de1dca02e77e9af7608
Steave-1125 <soccerplayer161236@gmail.com>
2020-10-21 17:23:59 +0900

    2020[秋学期]第4回の練習1

--- 20201014.js
+++ 20201014.js
@@ -1,2 +1,12 @@
-console.log("ほげほげ");
-console.log("ふがふが");
+let variableA;
+variableA = "最初の代入A";
+console.log(variableA);
+
+let variableB = "最初の代入B";
+console.log(variableB);
+
+variableA = "２番目の代入A";
+console.log(variableA);
+
+variableA = variableB;
+console.log(variableA);

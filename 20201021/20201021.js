05ab3c593c2b51a29e56a6636e18d47280998d00
Steave-1125 <soccerplayer161236@gmail.com>
2020-10-28 17:33:59 +0900

    更新

--- 20201021/20201021.js
+++ 20201021/20201021.js
@@ -1,6 +1,20 @@
-let numA = 10;
-let numB = 100;
-let numC = 50;
+// let numA = 10;
+// let numB = 100;
+// let numC = 50;
 
-console.log(numA + numB);
-console.log(numB / numA + numC);
+// let numA = "20";
+// let numB = "20";
+//
+// console.log(numA + numB);
+// console.log((numA + numB) + "hogehoge");
+// console.log(1000 + "fugafuga");
+// console.log("fugafuga" + 1000);
+
+// let numC = 10;
+// let ans = numA + numB * numC;
+// console.log(ans);
+
+let numA = 123;
+let numB = 20;
+console.log("文字列 ${numA} ${numB}");
+console.log(`文字列${numA} ${numB}`);
---
+++ 20201021/function-test.js
@@ -0,0 +1,24 @@
+// function sayHello(hello,hi,hey){
+//   console.log("Hello.");
+// }
+//
+// function sayhi(message){
+//   console.log(`あいさつ：${message}`);
+// }
+//
+// function sayinfo(name,old,from){
+//   console.log(`name: ${name}`,`old = ${old}`,`from = ${from}`);
+// }
+//
+// function sayfood(like,notlike= "default"){
+//   console.log(`like: ${like}`,`notlike: ${notlike}`);
+// }
+
+function math(){
+  return 2018*1010;
+}
+
+let ans = math();
+console.log(ans);
+
+console.log(ans);

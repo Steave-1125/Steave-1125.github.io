917da781b14c60ebc60cf780320d093ea2ea511b
Steave-1125 <soccerplayer161236@gmail.com>
2021-01-13 00:23:02 +0900

    2020年　最終課題

--- Lunchboard/slidshow.js
+++ Lunchboard/slidshow.js
@@ -1,4 +1,4 @@
-const food = ["img/beef.png","img/corry.png","img/karaage.png","img/kushikatsu.png"];
+const food = ["https://github.com/Steave-1125/Steave-1125.github.io/blob/master/Lunchboard/img/beef.png?raw=true","https://github.com/Steave-1125/Steave-1125.github.io/blob/master/Lunchboard/img/corry.png?raw=true","https://github.com/Steave-1125/Steave-1125.github.io/blob/master/Lunchboard/img/karaage.png?raw=true","https://github.com/Steave-1125/Steave-1125.github.io/blob/master/Lunchboard/img/kushikatsu.png?raw=true"];
 let num = -1;
 
 
